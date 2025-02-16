68512d7e64fe3a6eafbb396144a64be9841e853a
Sajaldwivedi <127932657+Sajaldwivedi@users.noreply.github.com>
2025-02-16 15:18:17 +0530

    updated

--- script.js
+++ script.js
@@ -1,87 +1,101 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const playerBar = document.querySelector(".playerbar");
-    const playPauseButton = document.querySelector(".playpausebutton img");
-    const songImg = document.getElementById("midsongimg");
-    const songInfo = document.getElementById("songinformation");
     let currentAudio = null;
+    let currentSongIndex = 0;
+    
+    const songs = [
+        { id: "songplayer1", name: "Uyii Amma (From Azaad)", img: "Images/Uyii Amma.jpg" },
+        { id: "songplayer2", name: "Ye Tune Kya Kiya", img: "Images/YE tune kya kiya.jpg" },
+        { id: "songplayer3", name: "Taras (From Munjya)", img: "Images/Tarsa.jpg" },
+        { id: "songplayer4", name: "Tere Vaaste", img: "Images/Tere Vaaste.jpg" },
+        { id: "songplayer5", name: "Tu Hai To Main Hoon", img: "Images/Tu Hai To Main Hoon.jpg" },
+        { id: "songplayer6", name: "Husn", img: "Images/Husn.jpg" },
+        { id: "songplayer7", name: "Nadaaniyan", img: "Images/Nadaaniyan.jpg" },
+        { id: "songplayer8", name: "Naina", img: "Images/Naina.jpg" },
+        { id: "songplayer9", name: "O Maahi", img: "Images/O Maahi.jpg" },
+        { id: "songplayer10", name: "Pehle Bhi Main", img: "Images/Pehle Bhi Main.jpg" },
+        { id: "songplayer11", name: "Sajna Ve Sajna", img: "Images/Sajna Ve Sajna.jpg" },
+        { id: "songplayer12", name: "Sajna", img: "Images/Sajna.jpg" },
+        { id: "songplayer13", name: "O Saki Saki", img: "Images/O Saki Saki.jpg" },
+        { id: "songplayer14", name: "Ishq Hai", img: "Images/Ishq Hai.jpg" },
+        { id: "songplayer15", name: "Aaj Ki Raat", img: "Images/Aaj ki Raat.jpg" },
+        { id: "songplayer16", name: "Akhiyan Gulaab", img: "Images/Akhiyan Gulaab.jpg" },
+        { id: "songplayer17", name: "Apna Bana Le", img: "Images/Apna Bana le.jpg" },
+        { id: "songplayer18", name: "Besharam Rang", img: "Images/Besharam Rang.jpg" },
+        { id: "songplayer19", name: "Morni", img: "Images/Morni.jpg" },
+        { id: "songplayer20", name: "Kissik", img: "Images/Kissik.jpg" },
+        { id: "songplayer21", name: "Jhoome Jo Pathan", img: "Images/Jhoome Jo Pathan.jpg" },
+        { id: "songplayer22", name: "Mein To Aayi Vrindavan Dhaam", img: "Images/mein to aayi.jpg" },
+        { id: "songplayer23", name: "Heeriye", img: "Images/Heeriye.jpg" },
+        { id: "songplayer24", name: "Dilbar", img: "Images/Dilbar.jpg" },
+        { id: "songplayer25", name: "Chaleya", img: "Images/Chaleya.jpg" },
+        { id: "songplayer26", name: "Tauba Tauba", img: "Images/Tauba Tauba.jpg" },
+        { id: "songplayer27", name: "Tere Naam", img: "Images/tere naam.jpg" }
+    ];
 
-    function setupAudio(playButtonId, audioId, songName, songImgSrc) {
-        const playButton = document.getElementById(playButtonId);
-        const audio = document.getElementById(audioId);
+    const songPlayerBar = document.querySelector(".playerbar");
+    const playPauseButton = document.querySelector(".playpausebutton img");
+    const prevButton = document.querySelector(".prevbutton img");
+    const nextButton = document.querySelector(".nextbutton img");
+    const songTitle = document.getElementById("songinformation");
+    const songImage = document.getElementById("midsongimg");
 
-        if (playButton && audio) {
-            playButton.addEventListener("click", function (event) {
-                event.preventDefault();
-                event.stopPropagation();
+    function playSong(index) {
+        if (currentAudio) {
+            currentAudio.pause();
+            currentAudio.currentTime = 0;
+        }
 
-                if (currentAudio && currentAudio !== audio) {
-                    currentAudio.pause();
-                    currentAudio.currentTime = 0;
-                }
+        currentSongIndex = index;
+        currentAudio = document.getElementById(songs[index].id);
 
-                if (audio.paused) {
-                    audio.play();
-                    showPlayer(songName, songImgSrc, audio);
-                } else {
-                    audio.pause();
-                    hidePlayer();
-                }
+        if (currentAudio) {
+            currentAudio.play();
+            songTitle.textContent = songs[index].name;
+            songImage.src = songs[index].img;
+            playPauseButton.src = "Images/pause_24dp_FFFF_FILL0_wght400_GRAD0_opsz24.svg";
+            songPlayerBar.classList.remove("hidden");
 
-                currentAudio = audio;
-            });
-
-            audio.addEventListener("ended", function () {
-                hidePlayer();
+            currentAudio.addEventListener("ended", function () {
+                nextSong();
             });
         }
     }
 
-    function showPlayer(songName, songImgSrc, audio) {
-        playerBar.classList.add("show");
-        songInfo.textContent = songName;
-        songImg.src = songImgSrc;
-        playPauseButton.src = "Images/pause_24dp_FFFF_FILL0_wght400_GRAD0_opsz24.svg";
-
-        playPauseButton.onclick = function () {
-            if (audio.paused) {
-                audio.play();
+    function togglePlayPause() {
+        if (currentAudio) {
+            if (currentAudio.paused) {
+                currentAudio.play();
                 playPauseButton.src = "Images/pause_24dp_FFFF_FILL0_wght400_GRAD0_opsz24.svg";
             } else {
-                audio.pause();
+                currentAudio.pause();
                 playPauseButton.src = "Images/play_arrow_24dp_FFFF_FILL0_wght400_GRAD0_opsz24.svg";
             }
-        };
+        }
     }
 
-    function hidePlayer() {
-        playerBar.classList.remove("show");
+    function prevSong() {
+        if (currentSongIndex > 0) {
+            playSong(currentSongIndex - 1);
+        }
     }
 
-    setupAudio("playbutton1", "songplayer1", "Uyii Amma (From Azaad)", "Images/Uyii Amma.jpg");
-    setupAudio("playbutton2", "songplayer2", "Ye Tune Kya Kiya", "Images/YE tune kya kiya.jpg");
-    setupAudio("playbutton3", "songplayer3", "Taras (From Munjya)", "Images/Tarsa.jpg");
-    setupAudio("playbutton4", "songplayer4", "Tere Vaaste", "Images/Tere Vaaste.jpg");
-    setupAudio("playbutton5", "songplayer5", "Tu Hai To Main Hoon", "Images/Tu Hai To Main Hoon.jpg");
-    setupAudio("playbutton6", "songplayer6", "Husn", "Images/Husn.jpg");
-    setupAudio("playbutton7", "songplayer7", "Nadaaniyan", "Images/Nadaaniyan.jpg");
-    setupAudio("playbutton8", "songplayer8", "Naina", "Images/Naina.jpg");
-    setupAudio("playbutton9", "songplayer9", "O Maahi", "Images/O Maahi.jpg");
-    setupAudio("playbutton10", "songplayer10", "Pehle Bhi Main (From Animal)", "Images/Pehle Bhi Main.jpg");
-    setupAudio("playbutton11", "songplayer11", "Sajna Ve Sajna", "Images/Sajna Ve Sajna.jpg");
-    setupAudio("playbutton12", "songplayer12", "Sajna", "Images/Sajna.jpg");
-    setupAudio("playbutton13", "songplayer13", "O Saki Saki", "Images/O Saki Saki.jpg");
-    setupAudio("playbutton14", "songplayer14", "Ishq Hai", "Images/Ishq Hai.jpg");
-    setupAudio("playbutton15", "songplayer15", "Aaj Ki Raat (From Stree 2)", "Images/Aaj ki Raat.jpg");
-    setupAudio("playbutton16", "songplayer16", "Akhiyan Gulaab", "Images/Akhiyan Gulaab.jpg");
-    setupAudio("playbutton17", "songplayer17", "Apna Bana Le (From Bhediya)", "Images/Apna Bana le.jpg");
-    setupAudio("playbutton18", "songplayer18", "Besharam Rang (From Pathaan)", "Images/Besharam Rang.jpg");
-    setupAudio("playbutton19", "songplayer19", "Morni", "Images/Morni.jpg");
-    setupAudio("playbutton20", "songplayer20", "Kissik", "Images/Kissik.jpg");
-    setupAudio("playbutton21", "songplayer21", "Jhoome Jo Pathaan", "Images/Jhoome Jo Pathan.jpg");
-    setupAudio("playbutton22", "songplayer22", "Mein To Aayi Vrindavan Dhaam", "Images/mein to aayi.jpg");
-    setupAudio("playbutton23", "songplayer23", "Heeriye", "Images/Heeriye.jpg");
-    setupAudio("playbutton24", "songplayer24", "Dilbar", "Images/Dilbar.jpg");
-    setupAudio("playbutton25", "songplayer25", "Chaleya", "Images/Chaleya.jpg");
-    setupAudio("playbutton26", "songplayer26", "Tauba Tauba (From Bad Newz)", "Images/Tauba Tauba.jpg");
-    setupAudio("playbutton27", "songplayer27", "Tere Naam", "Images/tere naam.jpg");
+    function nextSong() {
+        if (currentSongIndex < songs.length - 1) {
+            playSong(currentSongIndex + 1);
+        }
+    }
+
+    playPauseButton.addEventListener("click", togglePlayPause);
+    prevButton.addEventListener("click", prevSong);
+    nextButton.addEventListener("click", nextSong);
+
+    songs.forEach((song, index) => {
+        const playButton = document.getElementById(`playbutton${index + 1}`);
+        if (playButton) {
+            playButton.addEventListener("click", function (event) {
+                event.preventDefault();
+                playSong(index);
+            });
+        }
+    });
 });
